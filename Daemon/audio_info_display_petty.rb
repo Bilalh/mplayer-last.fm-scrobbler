f5462d4a4e377b29d4fe3328c8b7e0943bf65f42
Bilal Hussain <bhterra@gmail.com>
2011-11-09 03:18:31 +0000

    printing

---
+++ Daemon/audio_info_display_petty.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby19  -wKU
+require 'escape'
+
+output = $stderr
+
+`echo "get_property path" >>  ~/.mplayer/pipe`
+sleep 0.1
+filepath_with_name = `tail -n1 ~/.mplayer/output`
+filepath = filepath_with_name[/.*?=(.*)/,1]
+
+`echo "get_time_pos" >>  ~/.mplayer/pipe`
+sleep 0.1
+time_with_name = `tail -n1 ~/.mplayer/output`
+time = time_with_name[/.*?=(.*)/,1]
+time = time.to_f.round
+return unless time && filepath
+output.puts('# ' + `taginfo --pretty #{Escape.shell_command [filepath]} #{time}  2>/dev/null`)
